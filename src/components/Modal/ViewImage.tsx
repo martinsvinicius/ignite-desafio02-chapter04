5297beb8437dfb607d70f1ad0e20aa2bceb0e3f0
Vinicius Martins <vinicius.martins@parnamirim.rn.gov.br>
2021-08-31 12:06:24 -0300

    ViewImage modal done

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -18,6 +18,32 @@ export function ModalViewImage({
   isOpen,
   onClose,
   imgUrl,
-}: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+}: ModalViewImageProps) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent
+        bgColor="gray.900"
+        mx="auto"
+        w="auto"
+        h="auto"
+        maxW={['320px', '540px', '900px']}
+        maxH={['360px', '440px', '600px']}
+      >
+        <ModalBody p="0">
+          <Image
+            src={imgUrl}
+            maxW={['320px', '540px', '900px']}
+            maxH={['360px', '440px', '600px']}
+          />
+        </ModalBody>
+
+        <ModalFooter h="2" bg="gray.800" borderBottomRadius="6px">
+          <Link target="_blank" href={imgUrl} mr="auto" ml="10px">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
